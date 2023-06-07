913f4ad27719042c2633a97c4d1eea7773c4b898
allan <allankipchumba76@gmail.com>
2023-06-07 19:35:28 +0300

    {

--- controlers/post.js
+++ controlers/post.js
@@ -1,7 +1,34 @@
-export const getPosts = (req, res) => {};
+import { db } from "../db.js";
+
+//fetch all posts
+export const getPosts = (req, res) => {
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat=?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
+
+    return res.status(200).json(data);
+  });
+};
+
+//fetch a single post
 export const getPost = (req, res) => {};
+
+//add a post
 export const addPost = (req, res) => {
-  res.json("from controler");
+  const { title, desc, date, cat, uid } = req.body;
+
+  const q =
+    "INSERT INTO posts (`title`, `desc`, `date`, `uid`, `cat`) VALUES (?)";
+  const values = [title, desc, date, uid, cat];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json(data);
+  });
 };
 export const deletePost = (req, res) => {};
 
